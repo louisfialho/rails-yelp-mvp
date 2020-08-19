a93c20ec37f295959ecbcde6e636f9551745c1dd
Louis Fialho <louis.fialho@hec.edu>
2020-08-19 16:19:46 +0100

    git init

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,6 @@
 class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+
+  validates :name, :address, :category, presence: true
+  validates_inclusion_of :category, in: %w( chinese italian japanese french belgian )
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# db/seeds.rb
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", phone_number: "80923480", category: "french" }
+pizza_east =  { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", phone_number: "80923480", category: "japanese" }
+big_mamma =  { name: "Big Mamma", address: "9 Jane Street", phone_number: "80923480", category: "italian" }
+ombu =  { name: "Ombu", address: "489 Louis Street", phone_number: "80923480", category: "belgian" }
+silver_tower =  { name: "Silver Tower", address: "485 Highway Hills", phone_number: "80923480", category: "chinese" }
+
+[ dishoom, pizza_east, big_mamma, ombu, silver_tower ].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
+
